349199d05c465a5fa48549405061b527284f9606
Cristiano Junior Larrea Leal <ntconsult.leal@agibank.com.br>
2021-11-03 08:52:41 -0300

    Dinamic URL and Redirect

--- fgv/urls.py
+++ fgv/urls.py
@@ -3,6 +3,9 @@ from fgv import views as views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('special/', views.special, name='special'),
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('redireciona/', views.redireciona, name="redireciona"),
+    path('special/', views.retorna_html, name='retorna-html'),
+    path('special/<int:param>', views.view_dinamic_int, name='dinamica-int'),
+    path('special/<str:param>', views.view_dinamic_str, name='dinamica-str')
 ]
--- fgv/views.py
+++ fgv/views.py
@@ -1,8 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 def index(request):
     return HttpResponse("<strong>FGV</strong>")
 
-def special(request):
+def redireciona(request):
+    url_direcionamento = reverse("dinamica-str", args=['leao'])
+    return HttpResponseRedirect(url_direcionamento)
+
+def retorna_html(request):
     return render(request, 'fgv/fgv.html')
+
+def view_dinamic_int(request, param):
+    if param == 0:
+        return HttpResponse('<strong>Parâmetro 0</strong>')
+    elif param == 1:
+        return HttpResponse('<strong>Parâmetro 1</strong>')
+    elif param == 2:    
+        return HttpResponse('<strong>Parâmetro 2</strong>')
+    else:
+        return HttpResponseNotFound('Página não existe')
+
+def view_dinamic_str(request, param):
+    if param == 'leao':
+        return HttpResponse('<strong>Você se acha</strong>')
+    else:
+        return HttpResponseNotFound('Página não existe')
